c40e6394387dc81f16d8ad8532aac892572f7f8a
Brayan Portela <32397114+bjportelac@users.noreply.github.com>
2023-11-17 21:00:58 -0500

    Feat: Swapped Grammar to Golang
    - Enabled Golang as input grammar
    - Added basic visitor structures
    - Added input.txt for testing
    
    *Pending:* Implement control error to assert during translation.

--- src/Main.java
+++ src/Main.java
@@ -4,45 +4,27 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) throws Exception{
         try {
             GoControl goControl = new GoControl();
-            /*
-            Scanner scanner = new Scanner(System.in);
-            ArrayList<String> code = new ArrayList<>();
 
-            while (scanner.hasNextLine()) {
-                String line= scanner.nextLine();
-                code.add(line);
-            }
+            GoLexer goLexer = new GoLexer(CharStreams.fromFileName("input/input.txt"));
+            CommonTokenStream tokenStream = new CommonTokenStream(goLexer);
 
-            String program = String.join("\n", code);
+            GoParser goParser = new GoParser(tokenStream);
+            ParseTree goTree = goParser.sourceFile();
 
-            scanner.close();
-            code.clear();
-
-
-            TypeScriptLexer TSlexer = new TypeScriptLexer(CharStreams.fromString(program));*/
-            TypeScriptLexer TSlexer = new TypeScriptLexer(CharStreams.fromFileName("input/input.txt"));
-            CommonTokenStream tokenStream = new CommonTokenStream(TSlexer);
-
-            TypeScriptParser TSparser = new TypeScriptParser(tokenStream);
-            ParseTree tree = TSparser.program();
-
-            Translator loader = new Translator();
-            loader.visit(tree);
+            Translator<Object> loader = new Translator<>();
+            loader.visit(goTree);
 
             List<String> translation = Translator.translatedCode;
-            translation.toString();
 
-            /*Move translation to string interpreter to handle indentation
-             and new lines*/
-
-            String goLangCode = goControl.IndentationController(translation);
-            System.out.println(goLangCode);
+            String objectiveCode = goControl.IndentationController(translation);
+            System.out.println(objectiveCode);
 
         } catch (Exception e){
             System.err.println("Error during translation: " + e.getCause());
